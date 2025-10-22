46da858ff9ac2b0fe491bba901ccb4201abdcc49
ThiagoMoro <thiago.g.moro@gmail.com>
2025-10-22 23:11:53 +0100

    Initial header

--- links/src/app/index/index.tsx
+++ links/src/app/index/index.tsx
@@ -1,11 +1,17 @@
-import { Text, View } from "react-native"
+import { Image, View, TouchableOpacity } from "react-native"
+import { MaterialIcons } from "@expo/vector-icons"
 import { styles } from "./styles"
+import { colors } from "@/styles/colors"
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}> Hello </Text>
-      <Text style={styles.title}> Thiago </Text>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+        <TouchableOpacity>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
     </View>
   )
 }
